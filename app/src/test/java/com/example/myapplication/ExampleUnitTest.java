83124cd6e48b146728f10803a37d6453f250fd17
AmartiSamia <samia.elamarti11@gmai.com>
2025-11-24 09:40:39 +0100

    Make unit test fail

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -10,9 +10,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
- @Test
-public void addition_isCorrect() {
-    assertEquals(5, 2 + 2); // volontairement faux
-}
-
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
